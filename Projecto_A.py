c01817027dbf0ab0cb0663061beebd82de98829f
malmaboy <58644254+malmaboy@users.noreply.github.com>
2020-06-02 21:21:47 +0100

    Bases

--- Projecto_A.py
+++ Projecto_A.py
@@ -1,2 +1,38 @@
-print("Hello World")
+def Main():
+    MainMenu()
+    
+    
+    
+    
+    
+def MainMenu():
+    
+    print("What problem do you want to solve? ")
+    print("1. Floatation ")
+    print("2. Springs")
+    
+    
+    if input() == "1":
+        Problem1()
+    elif input()== "2":
+        Problem2()
+        
 
+def Problem1():
+    Gravidade = input()
+    Densidade_Fluido = input()
+    Densidade_Objecto = input()
+    Volume = input()
+    
+    
+    
+    
+    
+        
+def Problem2():
+    Gravidade2 = input()
+    Massa_Objecto = input()
+    Comprimento = input()
+    Constate = input()
+
+Main()
