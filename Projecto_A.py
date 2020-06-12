507d0cea0d69686487b9e3631a8d3292c689042b
Hugo Carvalho <huguinho66@live.com>
2020-06-12 21:18:07 +0100

    update

--- Projecto_A.py
+++ Projecto_A.py
@@ -18,36 +18,81 @@ def MainMenu():
         Problem2()
         
 
-def Problem1():
-    Gravidade = input()
-    Densidade_Fluido = input()
-    Densidade_Objecto = input()
-    Volume = input()
-    resultado
+def Problem1(): ## Flutuação 
+
+    print("Insert de Gravity: ")
+    G = float(input()) # Gravidade
+    print("Insert ")
+    DF = float(input()) # Densidade do Fluido
+    DO = float(input()) # Densidade do Objeto
+    V = float(input() # Volume 
+    
+
+    ## Formula Fb = P.G.V
 
     while (True):
-        if():
+        print("Object Properties: Mass =" + str() + "Density=" + str() +  "Volume ="+ str(V))
+        print("Fluid has density of " + str() + "Gravity is" + str())
+         
+        
+        
 
-
-        elif (input == "Exit"):
-            exit()
-    
     
     
     
     
         
-def Problem2():
+def Problem2(): ## Molas
+    
+    print("Insert the Gravity: ")
+    ForçaGravítica = float(input()) # gravidade 
+    print("Insert de Mass: ")
+    M = float(input()) # Massa do corpo
+    print("Insert the Constant: ")
+    K = float(input()) # Constante de mola
+    print("Insert the base spring length: ")
+    R = float(input()) # Comprimento da mola em descanso 
+    L = float() # Comprimento da mola esticada  
+    Fg = float() # força gravitica 
+    Fg = M * ForçaGravítica # Força Gravítica 
+    add = float() # Cálculos axilares
 
-    Gravidade2 = input()
-    Massa_Objecto = input()
-    Comprimento = input()
-    Constate = input()
+    print(Fg)
+    
+    while (True):
+        
 
-    while ( True):
-         if():
+        print("Object mass is: " + str(M), "Gravity is: " + str(ForçaGravítica))
+        print("Base spring length is: " + str(R), "Constant = " + str(K)) 
 
-    elif (input == "Exit"):
-            exit()
+        add = (- K * -R) + -Fg ## formula 
+        print(add)
+        L = add / K
+        print("Spring would stretch to: " + str(L))
+        
+        response = input()
+
+        if(response == "Set Mass ".split()):
+            M = float(response) 
+            print(M)
+        elif(response == "Set Constant ".split()):
+            K = response
+        elif(response == "Set Gravity" ):
+            ForçaGravítica = response
+        elif( response == "Set Length "):    
+            R = response
+        
+        
+
+        
+
+        
+
+
+        
+        
+
+    
+
 
 Main()
